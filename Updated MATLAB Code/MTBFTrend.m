fad36a15acedc51d31f0c50cc01710f775f22149
CannedLettuce <CannedLettuce@tutanota.com>
2021-11-26 14:20:53 -0700

    added correlate fucntion

--- Updated MATLAB Code/MTBFTrend.m
+++ Updated MATLAB Code/MTBFTrend.m
@@ -1,17 +1,39 @@
-table = csvread('allMTBF.csv');
 
-newTable = normalize(table);
 
-newTable = zeros(size(table,1),size(table,2));
+function newTable = normalize(table)
 
-for i = 1:size(table,1)
-    row = table(i,1:end);
-    s = 0;
-    for j = 1:size(table,2)
-        s = s + row(j);
+    newTable = zeros(size(table,1),size(table,2));
+
+    for i = 1:size(table,1)
+
+        row = table(i,1:end);
+
+        s = 0;
+
+        for j = 1:size(table,2)
+
+            s = s + row(j);
+
+        end
+
+        row = row / s;
+
+        newTable(i,1:size(table,2)) = row;
+
     end
-    row = row / s;
-    newTable(i,1:size(table,2)) = row;
-end
 
-surf(newTable)
+function R = correlate(table)
+
+    R = zeros(56,56);
+
+    for i = 1:56
+
+        for j = 1:56
+
+        [val index] = max(xcorr(table(i,1:end),table(j,1:end)));
+
+        R(i,j) = index;
+
+        end
+
+    end 
